bc12074174e374147d8f956a68a3d16c1df03b56
sabrito1912 <20253ds090@utez.edu.mx>
2026-01-21 08:44:16 -0600

    merge: cambio para conflicto

--- U1GitConflicto/src/Main.java
+++ U1GitConflicto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static final String VERSION_OBJETIVO = "PENDIENTE";
+    public static final String VERSION_OBJETIVO = "merge";
 
     public static void main(String[] args) {
         System.out.println("Version: " + VERSION_OBJETIVO);
