ba8b53271959ddc191093723c6e597689afd00ca
sabrito1912 <20253ds090@utez.edu.mx>
2026-01-21 08:37:40 -0600

    init: proyecto U1GitConflicto

---
+++ U1GitConflicto/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static final String VERSION_OBJETIVO = "PENDIENTE";
+
+    public static void main(String[] args) {
+        System.out.println("Version: " + VERSION_OBJETIVO);
+    }
+}
