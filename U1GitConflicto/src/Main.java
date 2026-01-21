0a250469a88b13dd7404af0a6f2df97c3e088527
sabrito1912 <20253ds090@utez.edu.mx>
2026-01-21 08:51:13 -0600

    develop: cambio para conflicto

--- U1GitConflicto/src/Main.java
+++ U1GitConflicto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static final String VERSION_OBJETIVO = "PENDIENTE";
+    public static final String VERSION_OBJETIVO = "develop";
 
     public static void main(String[] args) {
         System.out.println("Version: " + VERSION_OBJETIVO);
